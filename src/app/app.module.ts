7c8cc62eb2762d6b283bdc56ce32f51768f09729
i-zeshawn <zeeshahmed.dev@gmail.com>
2022-06-20 13:03:31 +0500

    model patched with options

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FormBodyComponent } from './form-body/form-body.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {FormBodyComponent} from './form-body/form-body.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatIconModule} from "@angular/material/icon";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
@@ -13,6 +13,8 @@ import {EditorModule} from "@tinymce/tinymce-angular";
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatSlideToggleModule} from "@angular/material/slide-toggle";
 import {MatButtonModule} from "@angular/material/button";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatSelectModule} from "@angular/material/select";
 
 @NgModule({
   declarations: [
@@ -30,9 +32,12 @@ import {MatButtonModule} from "@angular/material/button";
     EditorModule,
     MatCheckboxModule,
     MatSlideToggleModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/form-body/form-body.component.ts
+++ src/app/form-body/form-body.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import Swal from 'sweetalert2'
+import {Options} from "../models/Options";
 
 @Component({
   selector: 'app-form-body',
@@ -20,10 +23,46 @@ export class FormBodyComponent implements OnInit {
       bullist numlist outdent indent | removeformat | help'
   };
 
-  constructor() {
+  carForm: any;
+
+  options = new Options();
+
+  constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
+
+    this.createForm();
+    // Swal.fire({
+    //   title: 'Error!',
+    //   text: 'Do you want to continue',
+    //   icon: 'info',
+    //   confirmButtonText: 'OK'
+    // })
   }
 
+  private createForm() {
+    this.carForm = this.formBuilder.group({
+      category_id: [1],
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      transmission: ['', Validators.required],
+      fuel_type: ['', Validators.required],
+      built_year: ['', Validators.required],
+      color: [''],
+      mileage: [''],
+      maintained_by_company: [''],
+      price_type: [],
+      price: [],
+      name: [''],
+      email: [''],
+      phone_number: [''],
+      show_whatsapp: [0],
+      whatsapp: [],
+      city: [''],
+      area: [''],
+      options: []
+
+    })
+  }
 }
